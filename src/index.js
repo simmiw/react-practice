c7ee372a73a2b313c88d156151dded0ea97d0632
Simmi Walia <simmiw1994@gmail.com>
2024-02-21 20:53:40 +0530

    Integarting react redux in project

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<BrowserRouter><App /></BrowserRouter>);
+root.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </BrowserRouter>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
